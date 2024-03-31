6982040a74f72a8be54d09f9dee235587364c058
muicoder <muicoder@gmail.com>
2024-03-31 12:03:18 +0800

    release-1.26

--- cmd/cert/main.go
+++ cmd/cert/main.go
@@ -1,27 +1,23 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"os"
 
 	"github.com/k3s-io/k3s/pkg/cli/cert"
 	"github.com/k3s-io/k3s/pkg/cli/cmds"
 	"github.com/k3s-io/k3s/pkg/configfilearg"
-	"github.com/sirupsen/logrus"
-	"github.com/urfave/cli"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
 	app := cmds.NewApp()
-	app.Commands = []cli.Command{
+	app.Commands = []*cli.Command{
 		cmds.NewCertCommands(
+			cert.Check,
 			cert.Rotate,
 			cert.RotateCA,
 		),
 	}
 
-	if err := app.Run(configfilearg.MustParse(os.Args)); err != nil && !errors.Is(err, context.Canceled) {
-		logrus.Fatal(err)
-	}
+	cmds.MustRun(app, configfilearg.MustParse(os.Args))
 }
