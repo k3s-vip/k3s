12869792e3ea20e630994014c8c73d07785c9159
muicoder <muicoder@gmail.com>
2024-03-31 12:03:18 +0800

    release-1.26

--- cmd/containerd/main.go
+++ cmd/containerd/main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/k3s-io/k3s/pkg/containerd"
-	"k8s.io/klog"
+	"k8s.io/klog/v2"
 )
 
 func main() {
 	klog.InitFlags(nil)
+	klog.EnableContextualLogging(true)
 	containerd.Main()
 }
