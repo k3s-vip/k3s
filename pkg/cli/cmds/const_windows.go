344025330dc1e3ffb012fa11dc259f0720eaa5ec
muicoder <muicoder@gmail.com>
2024-03-31 12:03:18 +0800

    release-1.26

--- pkg/cli/cmds/const_windows.go
+++ pkg/cli/cmds/const_windows.go
@@ -1,9 +1,7 @@
 //go:build windows
-// +build windows
 
 package cmds
 
 const (
-	DefaultPauseImage  = "mcr.microsoft.com/oss/kubernetes/pause:1.4.0"
-	DefaultSnapshotter = "native"
+	DefaultSnapshotter = "windows"
 )
