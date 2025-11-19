6467778f72214414694fd1e03f1eb18994623c39
muicoder <muicoder@gmail.com>
2025-11-19 18:34:52 +0800

    Revert
    wait.Poll
    wrangler

--- pkg/codegen/main.go
+++ pkg/codegen/main.go
@@ -4,8 +4,8 @@ import (
 	"os"
 
 	v1 "github.com/k3s-io/k3s/pkg/apis/k3s.cattle.io/v1"
-	controllergen "github.com/rancher/wrangler/v3/pkg/controller-gen"
-	"github.com/rancher/wrangler/v3/pkg/controller-gen/args"
+	controllergen "github.com/rancher/wrangler/pkg/controller-gen"
+	"github.com/rancher/wrangler/pkg/controller-gen/args"
 )
 
 var (
