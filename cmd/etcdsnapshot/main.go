31590863b5e77f02c17b6abcbf9d3cec05949f9d
muicoder <muicoder@gmail.com>
2026-03-02 11:19:41 +0800

    release-1.34

--- cmd/etcdsnapshot/main.go
+++ cmd/etcdsnapshot/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"context"
-	"errors"
 	"os"
 
 	"github.com/k3s-io/k3s/pkg/cli/cmds"
 	"github.com/k3s-io/k3s/pkg/cli/etcdsnapshot"
 	"github.com/k3s-io/k3s/pkg/configfilearg"
-	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
 
@@ -23,7 +20,5 @@ func main() {
 		),
 	}
 
-	if err := app.Run(configfilearg.MustParse(os.Args)); err != nil && !errors.Is(err, context.Canceled) {
-		logrus.Fatal(err)
-	}
+	cmds.MustRun(app, configfilearg.MustParse(os.Args))
 }
